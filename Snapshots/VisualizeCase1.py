a5bbd67493f9e863dfd23edc04f3fbcc52446f3a
mateorodfr <62598297+mateorodfr@users.noreply.github.com>
2020-04-29 10:06:48 +0200

    update

--- Snapshots/VisualizeCase1.py
+++ Snapshots/VisualizeCase1.py
@@ -6,6 +6,7 @@ import csv
 rawData = open(r"Snapshots\Case0.dat" , "r")
 lines = rawData.readlines()
 rawData.close()
+lx,ly, snapshots = [],[],[]
 
 airData = open(r"Snapshots\airfoil.csv" , "r")
 airlines = airData.readlines()
@@ -30,6 +31,8 @@ for masteri in range(100):
     xx = np.around(xx,2)
     yy = np.around(yy,2)
     zz = np.around(zz,2)
+    xsnap = []
+    ysnap = []
 
 
     zmin = 620
@@ -76,63 +79,39 @@ for masteri in range(100):
     xmean, ymean = cp.columnaverage(xcolsfinal, ycolsfinal)
 
     if len(xmean) == 8 and len(ymean) == 8:
-        xsnap.append(xmean)
-        ysnap.append(ymean)
+        lx.append(xmean)
+        ly.append(ymean)
+        snapshots.append(masteri)
 
+for i in range(len(lx)):
+    newrowx = sorted(lx[i])
+    newrowy = sorted(ly[i])
+    lx[i] = newrowx
+    ly[i] = newrowy
+print(lx)
 
+f = open('Snapshots\Case1Clean.csv', 'w')
 
-
-newxx = []
-newzz = []
-newyy = []
-for i in range(len(xsnap)):
-    newxx.extend(xsnap[i])
-    newyy.extend(ysnap[i])
-
-xsnapfinal, ysnapfinal = cp.datamakecolumn(newxx,newyy,3)
-xsnapfilt, ysnapfilt = cp.columnstdfilter(xsnapfinal,ysnapfinal)
-xsnapmean, ysnapmean = cp.columnaverage(xsnapfilt, ysnapfilt)
-coord = []
-for i in range(len(xsnapmean)):
-    coordi = [xsnapmean[i],ysnapmean[i]]
-    coord.append(coordi)
-
-f = open('Snapshots\Case0Clean.csv', 'w')
 with f:
     writer = csv.writer(f)
-    writer.writerows(coord)
-
-
-targetx = 385
-targety = -3.84116
-
-deltax = targetx - xsnapmean[-1]
-deltay = targety - ysnapmean[-1]
-print(deltay)
-xsnaptrans = []
-ysnaptrans = []
-for i in range(len(xsnapmean)):
-    xi = xsnapmean[i]+deltax
-    yi = ysnapmean[i] + deltay
-    xsnaptrans.append(xi)
-    ysnaptrans.append(yi)
-xlin = np.linspace(0,400)
-ylin = np.zeros(len(xlin))
+    for i in range(len(lx)):
+        snap = np.ones(len(lx[i]))
+        currentsnap = snap*int(i)
+        void = 'Snapshot' + str(i)
+        coord = [currentsnap,lx[i],ly[i]]
+        rowin = [void,'x', 'y']
+        writer.writerow(rowin)
+        writer.writerows(coord)
 
 
 
-
-#plt.ylim(-150,150)
-#plt.plot(airx,airy,'b--', label='airfoil')
-#plt.plot(xlin,ylin, 'b--', label='airfoil chord line')
-
-plt.scatter(newxx,newyy, label='filtered data for all snapshots in Case0')
-plt.scatter(xmean,ymean,color='r',label='average position of columns for all snapshots in Case0')
-plt.title(label='PIV Case0 filtered data and column average position')
-plt.xlabel('Position along chord x [mm]')
-plt.ylabel('Position along airfoil y axis (thickness) [mm]')
-plt.legend()
-plt.show()
+# plt.scatter(newxx,newyy, label='filtered data for all snapshots in Case0')
+# plt.scatter(xmean,ymean,color='r',label='average position of columns for all snapshots in Case0')
+# plt.title(label='PIV Case0 filtered data and column average position')
+# plt.xlabel('Position along chord x [mm]')
+# plt.ylabel('Position along airfoil y axis (thickness) [mm]')
+# plt.legend()
+# plt.show()
 
 
 
