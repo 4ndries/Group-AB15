ae08c3355375091092cdc24b41327f95ccd19d8d
mateorodfr <62598297+mateorodfr@users.noreply.github.com>
2020-05-04 19:13:17 +0200

    animation

--- Snapshots/Test.py
+++ Snapshots/Test.py
@@ -1,11 +1,28 @@
 import readCases as rc
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import animation
 
-snapshots,xsnapshots,ysnapshots = rc.readComplexCases('Snapshots\Case2TrackIDClean.csv')
-snapi = np.arange(len(xsnapshots)//11,len(xsnapshots)//10)
-for i in snapi:
-    plt.scatter(xsnapshots[i],ysnapshots[i], label='Snapshots %s' % snapshots[i])
+snapshots,xsnapshots,ysnapshots = rc.readComplexCases('Snapshots\Case2BTrackIDClean.csv')
+snapi = np.arange(0,len(xsnapshots))
 
+
+fig = plt.figure()
+ax = plt.axes(xlim=(xsnapshots[0][0]-50, xsnapshots[0][-1]+50), ylim=(ysnapshots[0][0]-50, ysnapshots[0][-1]+50))
+line, = ax.plot([], [],'ro', lw=3)
+def init():
+    line.set_data([],[])
+    return line,
+
+def animate(i):
+    x = xsnapshots[i]
+    y = ysnapshots[i]
+    line.set_data(x,y)
+    return line,
+
+plt.title('Case2 Movement')
+plt.axis('off')
+anim = animation.FuncAnimation(fig,animate,init_func=init,frames=5000,interval=20,blit=True)
+anim.save('Snapshots\Animation.gif', writer='imagemagick', fps=30)
 plt.show()
 
